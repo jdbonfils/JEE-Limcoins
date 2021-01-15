d9ba5211499947d769c0512e7409f2c2264d498b
jdbonfils <39585460+jdbonfils@users.noreply.github.com>
2021-01-15 13:53:46 +0100

    Delete currency.py

--- src/main/webapp/resources/currency.py
+++
@@ -1,8 +0,0 @@
-import requests,json
-
-resp = requests.get('https://free.currconv.com/api/v7/convert?q=USD_GBP,EUR_GBP&compact=ultra&apiKey=9a24447e9d9caf7e2111')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-print(float(resp.json()['USD_GBP']))
-print(float(resp.json()['EUR_GBP']))
